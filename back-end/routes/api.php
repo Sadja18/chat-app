6965047aa9b2ac622dfaeb817963d317fd8478c5
Naman Mishra <namanmishraec1045@gmail.com>
2023-06-08 17:25:55 +0530

    user backend API endpoints created

---
+++ back-end/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('/users')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login'])->name('users.login');
+    Route::post('/logout', [UserController::class, 'logout']);
+});
+
+Route::get('/no-auth', function () {
+    return json_encode(
+        array(
+            'message' => 'Unauthorized',
+            'error' => 'You are not using an active session',
+            'statusCode' => 401
+        )
+    );
+})->name('no-auth');
+
+Route::get('/', function () {
+    return json_encode(
+        array(
+            'message' => 'success',
+            'info' => 'Welcome to home'
+        )
+    );
+})->name('/welcome');
